527af8310d6dff620fdc8db9e79f3aafd2080776
David Heizer <DavidHeizer@QuickenLoans.com>
2020-10-27 10:11:42 -0400

    Scarpping Data done

---
+++ RecreationGovScrape/Models/RECDATA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecreationGovScrape.Models
+{
+    public class RECDATA
+    {
+        public string FacilityID { get; set; }
+        public string FacilityName { get; set; }
+        public string FacilityDescription { get; set; }
+        public string FacilityTypeDescription { get; set; }
+        public double FacilityLongitude { get; set; }
+        public double FacilityLatitude { get; set; }
+        public string FacilityPhone { get; set; }
+        public string FacilityEmail { get; set; }
+    }
+}
---
+++ RecreationGovScrape/Models/RIDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecreationGovScrape.Models
+{
+    public class RIDB
+    {
+        public RECDATA[] recData { get; set; }
+        public METADATA metaData { get; set; }
+    }
+
+
+    public class METADATA
+    {
+        public Results results { get; set; }
+    }
+
+    public class Results
+    {
+        public int current_count { get; set; }
+        public int total_count { get; set; }
+    }
+
+}
+
+
